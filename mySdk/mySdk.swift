e0f06ba596da314a6e09a2e57362464b38bfbb86
maaztausif <maaztausif48@gmail.com>
2026-05-23 01:09:04 +0200

    Initial Commit

---
+++ mySdk/mySdk.swift
@@ -0,0 +1,9 @@
+//
+//  mySdk.swift
+//  mySdk
+//
+//  Created by Maaz Bin Tausif on 23.05.26.
+//
+
+import Foundation
+
---
+++ mySdkTests/mySdkTests.swift
@@ -0,0 +1,17 @@
+//
+//  mySdkTests.swift
+//  mySdkTests
+//
+//  Created by Maaz Bin Tausif on 23.05.26.
+//
+
+import Testing
+@testable import mySdk
+
+struct mySdkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
